d83a64cc3cc890801a76c14fca7f9b2f3ca47d8c
Thế Anh Đặng <dangtheanh16@gmail.com>
2025-02-15 14:21:36 +0700

    feat: Update template code

--- webpack.mix.js
+++ webpack.mix.js
@@ -2,4 +2,15 @@ let mix = require('laravel-mix');
 
 mix.setPublicPath('dist');
 
-mix.combine('src/main.js', 'dist/js/main.min.js');
+// mix.combine('src/main.js', 'dist/js/main.min.js');
+
+mix.combine([
+    'src/js/owlcarousel2.min.js',
+    'src/js/sticky-sidebar.min.js',
+    'src/js/jquery-replacetext.min.js',
+    'src/js/toc.min.js',
+], 'dist/js/vendor.min.js');
+
+mix.combine('src/js/main.js', 'dist/js/main.min.js');
+
+mix.css('src/css/main.css', 'dist/css/main.min.css');
